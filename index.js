f63137ae808e9fd28ef24ea5cae14282e79fa4c3
Kunj Bhatia <kunj.bhatia@w3villa.com>
2025-12-31 14:25:01 +0530

    Add initial REST API documentation for user management endpoints

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+const PORT = 8000;
+
+//Routes
+
+app.get("/users" , (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `;
+    res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
